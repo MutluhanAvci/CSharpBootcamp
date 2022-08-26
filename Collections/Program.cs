737eebd5230951a663e30b3077b2cc315a863e98
Mutluhan Avci <65674782+MutluhanAvci@users.noreply.github.com>
2022-08-26 04:07:58 +0300

    Generics and Collections introductions

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Mutluhan", "Yılmaz", "Avcı", "Defne", "Fatoş" };
+            List<string> names2 = new List<string> { "Mutluhan", "Yılmaz", "Avcı", "Defne", "Fatoş" };
+            names2.Add("Özlem");
+            for (int i = 0; i < names2.ToArray().Length; i++)
+            {
+                Console.WriteLine(names2[i]);
+            }
+            
+
+        }
+    }
+}
